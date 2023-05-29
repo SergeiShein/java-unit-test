90ad1ef38339ec6fb96080a6e613c4feca8e3d80
Sergei <sergeyshein20@yandex.ru>
2023-05-29 14:40:29 +0100

    My unit tests

---
+++ task_3.java
@@ -0,0 +1,36 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals;
+
+@RunWith(Parameterized.class)
+public class CheckIsAdultTest {
+
+    private final int age;
+    private final boolean result;
+
+    public CheckIsAdultTest(int age, boolean result) {
+        this.age = age;
+        this.result = result;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] getTextData() {
+        return new Object[][] {
+                {21, true},
+                {19, true},
+                {17, false},
+                {18, true},
+        };
+    }
+
+    @Test
+    public void checkIsAdultWhenAgeThenResult() {
+        Program program = new Program();
+        // Передай сюда возраст пользователя
+        boolean isAdult = program.checkIsAdult(age);
+        // Сравни полученный и ожидаемый результаты, не забудь про сообщение об ошибке
+        assertEquals("Ожидаем другой результат", result, isAdult);
+    }
+}
